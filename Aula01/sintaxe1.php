a5fdd811093a743fc1301681432758862c8bead1
gabzin16 <gabrielg.silvasenai@gmail.com>
2023-08-10 12:43:53 -0300

    .

---
+++ Aula01/sintaxe1.php
@@ -0,0 +1,12 @@
+<?php
+    // Geração e texto (string)
+
+    echo "Chama Sesi-Senai!";
+
+    /* geração de texto estruturado (com tags, atributos)*/
+
+    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.</h2>"; 
+    echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP.</h2>";
+
+?>
+
